e446506d15c20ddeec9ee2ed82b2c1ef39004e24
Prashin Jethwa <pjethwa@ma023782.ads.eso.org>
2018-01-19 18:26:07 +0100

    change output to final version

--- grouping.py
+++ grouping.py
@@ -124,7 +124,7 @@ def get_one_chat_grouping_A(G, M, cats, idx_grp, test_groups, N_ppl, N_cat):
     groups = []
     idx_free_clq = np.where(df['n_met']>=0)[0]
     n_assigned_ppl = 0
-    while (n_assigned_ppl < int(0.6*N_ppl)) and (idx_free_clq.size > 0):
+    while (n_assigned_ppl < int(0.4*N_ppl)) and (idx_free_clq.size > 0):
 
         idx_clq0 = df.loc[idx_free_clq].index.tolist()[0]
         groups.append(df.loc[idx_clq0]['group'])
@@ -143,7 +143,7 @@ def get_one_chat_grouping_A(G, M, cats, idx_grp, test_groups, N_ppl, N_cat):
     N_unassigned = len(unassigned_ppl)
     if N_unassigned > N_per_grp:
         N_lo_grp = int(np.floor(N_unassigned/N_per_grp))
-        N_rnd = 300
+        N_rnd = 500
         max_cost_leftover = np.inf
         for i in range(N_rnd):
             tmp = np.random.permutation(unassigned_ppl)
@@ -231,7 +231,7 @@ def get_session_grouping_A(xl, idx_grp, G, M, cats):
 # Algorithm B #########################################
 
 def get_one_chat_grouping_B(G, M, cats, idx_grp, N_ppl, N_cat):
-    N_test = 2000
+    N_test = 5000
     max_cost = np.inf
     N_grps = np.floor(len(idx_grp)/N_per_grp)
     for i in range(N_test):
@@ -320,7 +320,6 @@ def concat_table_lists(grp_r1_s1, grp_r2_s1, grp_r1_s2, grp_r2_s2):
 
     return df
 
-# To make nice...
 from ast import literal_eval
 def print_personal_timetable(i_person, xl, plist, grp, outdir):
 
@@ -361,6 +360,168 @@ def print_personal_timetable(i_person, xl, plist, grp, outdir):
             tab = grp[idx]['table'][tab].iloc[0]
             f.write('\tchat {i}: table {j}\n'.format(i=ic+1, j=tab+1))
     f.close()
+
+    return 0
+
+def print_fancy_personal_timetable(i_person, xl, plist, grp, outdir):
+
+    if not os.path.isdir(outdir+'fancy_schedules/'):
+        os.mkdir(outdir+'fancy_schedules/')
+
+    # number of tables
+    nt_r1 = len(grp[(grp['room']==1) & (grp['chat']==0) & (grp['session']==1)])
+    nt_r2 = len(grp[(grp['room']==2) & (grp['chat']==0) & (grp['session']==1)])
+
+    # get room for session 1
+    if i_person in plist['r1s1']:
+        room_s1 = 1
+        room_s1_str = 'Fornax'
+        nt_s1 = nt_r1
+    else:
+        room_s1 = 2
+        room_s1_str = 'Pictor/Sculptor'
+        nt_s1 = nt_r2
+
+    # get room for session 2
+    if i_person in plist['r1s2']:
+        room_s2 = 1
+        room_s2_str = 'Fornax'
+        nt_s2 = nt_r1
+    else:
+        room_s2 = 2
+        room_s2_str = 'Pictor/Sculptor'
+        nt_s2 = nt_r2
+
+    t_s1 = np.zeros(N_chats_per_sesh)
+    t_s2 = np.zeros(N_chats_per_sesh)
+    arrays = zip([1,2], [room_s1,room_s2], [nt_s1,nt_s2], [t_s1, t_s2])
+    for (sesh, room, ntables, t) in arrays:
+        for ic in range(N_chats_per_sesh):
+            idx = ((grp['chat']==ic) & (grp['session']==sesh) & (grp['room']==room))
+            tab = []
+            for j in range(ntables):
+                check = np.isin([i_person], literal_eval(grp[idx]['group'].iloc[j]))
+                tab.append(check[0])
+            tab = grp[idx]['table'][tab].iloc[0]
+            t[ic] = tab
+
+    # not 0 index tables
+    t_s1 +=1
+    t_s2 +=1
+    # add 12 to room 2
+    if room_s1==2:
+        t_s1 += 12
+    if room_s2==2:
+        t_s2 += 12
+
+    width, height = 175., 265.
+    mm2inch = 25.4
+
+    import matplotlib
+    import matplotlib.font_manager as font_manager
+    font_dirs = ['/Library/Fonts']
+    font_files = font_manager.findSystemFonts(fontpaths=font_dirs)
+    font_list = font_manager.createFontList(font_files)
+    font_manager.fontManager.ttflist.extend(font_list)
+    matplotlib.rcParams['font.family'] = 'Helvetica Neue LT Com'
+
+    fig = plt.figure(figsize=(width/mm2inch, height/mm2inch), dpi=500)
+    renderer = fig.canvas.get_renderer()
+
+    outfile = outdir+'fancy_schedules/schedule_{0:03d}.pdf'.format(i_person)
+
+    from PIL import Image
+    eso_logo = Image.open('data/icons/eso-logo-p3005.jpg')
+    x0 = 0.
+    w0 = 1.7* 10.731 /width
+    h0 = 1.7* 14 /height
+    y0 = 1. - h0
+    ax_eso = fig.add_axes([x0, y0, w0, h0])
+    ax_eso.imshow(eso_logo)
+    ax_eso.axis('off')
+
+    ax_text = fig.add_axes([0, 0, 0.001, 0.001])
+    ax_text.axis('off')
+
+    # header
+    event_name = ax_text.text(
+            1.,
+            1.,
+            'ESO Garching Science Day',
+            ha='right',
+            va='top',
+            # fontname='Helvetica',
+            size=10,
+            weight='bold',
+            transform=fig.transFigure
+    )
+
+    bb = event_name.get_window_extent(renderer=renderer)
+    x0 = bb.x0 * 1./(bb.x0+bb.width)
+    y0 = bb.y0 * 1./(bb.y0+bb.height)
+    y0 -= 0.01
+    ax_text.text(
+            x0,
+            y0,
+            '24 January 2018',
+            ha='left',
+            va='top',
+            # fontname='Helvetica',
+            size=10,
+            weight='light',
+            transform=fig.transFigure
+    )
+
+    # body
+    ls = 0.025
+    kw = {  'ha':'center',
+            'va':'bottom',
+            'size':10,
+            'transform':fig.transFigure
+            }
+    def write_line(x0, y0, txt, weight, kw, linestep=1):
+        ax_text.text(x0, y0, txt, weight=weight, **kw)
+        return y0 - linestep*ls
+
+    x0, y0 = 0.5, 0.75
+    txt = 'Schedule for ' + xl.iloc[i_person]['Name']
+    y0 = write_line(x0, y0, txt, 'bold', kw, 1.5)
+
+    x0_time = 0
+    x0_text = 0.1
+    kw['ha']='left'
+
+    txt = '10:30'
+    y0 = write_line(x0_time, y0, txt, 'bold', kw, 0)
+    txt = 'Welcome'
+    y0 = write_line(x0_text, y0, txt, 'bold', kw, 1.5)
+
+    arrays = zip([1,2],
+                [room_s1_str,room_s2_str],
+                [t_s1, t_s2],
+                ['11:00', '14:00']
+                )
+    for (sesh, room, t, time) in arrays:
+        y0 = write_line(x0_time, y0, time, 'bold', kw, 0)
+        txt = 'Session {i} in {j}'.format(t=time, i=sesh, j=room)
+        y0 = write_line(x0_text, y0, txt, 'bold', kw)
+        for ic in range(N_chats_per_sesh):
+            txt = '{i}. Table {j}'.format(i=ic+1, j=int(t[ic]))
+            y0 = write_line(x0_text, y0, txt, 'light', kw)
+        y0 -= 0.5*ls
+        if sesh==1:
+            txt = '12:30'
+            y0 = write_line(x0_time, y0, txt, 'bold', kw, 0)
+            txt = 'Photo outside Fornax or on library stairs, followed by lunch in Fornax'
+            y0 = write_line(x0_text, y0, txt, 'bold', kw, 1.5)
+
+    txt = '15:30'
+    y0 = write_line(x0_time, y0, txt, 'bold', kw, 0)
+    txt = 'Beer and pretzels in Fornax'
+    y0 = write_line(x0_text, y0, txt, 'bold', kw)
+
+    fig.savefig(outfile)
+    plt.close()
 
     return 0
 
@@ -453,7 +614,6 @@ def plot_grouping_stats(grp, M, N_ppl, outdir):
     axhist.hist(allmet, range=(29.5, 48.5), bins=19, histtype='stepfilled')
     axhist.set_xlabel('Total people met')
 
-    #fig.savefig(outdir+'groupstats.png')
     fig.savefig('output/grouping/plots/'+outdir+'.png')
     plt.close()
 
@@ -476,8 +636,6 @@ def main(pars):
 
     # read participants
     xl = pd.read_excel('data/doodle.xls', header=3, skip_footer=1)
-    xl.loc['Ralf Siebenmorgen']['Spectroscopy'] = 'OK'
-    xl.loc['Ralf Siebenmorgen']['Stellar Structure & Evolution'] = 'OK'
     xl['Name'] = xl.index
     N_ppl = len(xl)
     N_cat =len(xl.iloc[0]) - 1
@@ -503,7 +661,6 @@ def main(pars):
         M = nx.Graph()
         M.add_nodes_from(np.arange(N_ppl))
 
-        # TO-DO: remove certain people from morning/afternoon session as requested
         print('Grouping session 1...')
         idx_r1_s1, idx_r2_s1 = split_in_two(N_ppl)
         print('   ... room 1...')
@@ -533,28 +690,60 @@ def main(pars):
         nx.write_edgelist(M, outdir+"meetings.edgelist", data=True)
         M = nx.read_edgelist(outdir+"meetings.edgelist")
 
-    # analyse/export grouping
+    # for i_person in range(N_ppl):
+    #     print_personal_timetable(i_person, xl, plist, groups, outdir)
     for i_person in range(N_ppl):
-        print_personal_timetable(i_person, xl, plist, groups, outdir)
+        print_fancy_personal_timetable(i_person, xl, plist, groups, outdir)
 
-    # plot_grouping_stats(groups, M, N_ppl, outdir.split('/')[2])
+    plot_grouping_stats(groups, M, N_ppl, outdir.split('/')[2])
 
     return 0
 
-parlist = [
-    (17373, 'A', 'output/grouping/A001/'),
-    (64755, 'A', 'output/grouping/A002/'),
-    (25734, 'A', 'output/grouping/A003/'),
-    (45851, 'A', 'output/grouping/A004/'),
-    (11173, 'A', 'output/grouping/A005/'),
-    (17373, 'B', 'output/grouping/B001/'),
-    (64755, 'B', 'output/grouping/B002/'),
-    (25734, 'B', 'output/grouping/B003/'),
-    (45851, 'B', 'output/grouping/B004/'),
-    (11173, 'B', 'output/grouping/B005/'),
-]
+# try lots of random variations
 
-# for pars in parlist:
-#     main(pars)
+# parlist = [
+#     (17373, 'A', 'output/grouping/A001/'), # algo A
+#     (64755, 'A', 'output/grouping/A002/'), # N_rnd = 300
+#     (25734, 'A', 'output/grouping/A003/'), # n_assigned_ppl < int(0.6*N_ppl)
+#     (45851, 'A', 'output/grouping/A004/'),
+#     (11173, 'A', 'output/grouping/A005/'),
+#     (17373, 'B', 'output/grouping/B001/'), # algo B
+#     (64755, 'B', 'output/grouping/B002/'), # N_test = 2000
+#     (25734, 'B', 'output/grouping/B003/'),
+#     (45851, 'B', 'output/grouping/B004/'),
+#     (11173, 'B', 'output/grouping/B005/'),
+#     (76754, 'A', 'output/grouping/C001/'), # algo A
+#     (94375, 'A', 'output/grouping/C002/'), # N_rnd = 500
+#     (67585, 'A', 'output/grouping/C003/'), # n_assigned_ppl < int(0.4*N_ppl)
+#     (11637, 'A', 'output/grouping/C004/'),
+#     (85746, 'A', 'output/grouping/C005/'),
+# ]
+# parlist = [
+#     (54545, 'A', 'output/grouping/D001/'), # algo A
+#     (47575, 'A', 'output/grouping/D002/'), # N_rnd = 500
+#     (23232, 'A', 'output/grouping/D003/'), # n_assigned_ppl < int(0.4*N_ppl)
+#     (68685, 'A', 'output/grouping/D004/'),
+#     (35737, 'A', 'output/grouping/D005/'),
+#     (38444, 'A', 'output/grouping/D006/'),
+#     (24953, 'A', 'output/grouping/D007/'),
+#     (45353, 'A', 'output/grouping/D008/'),
+#     (53535, 'A', 'output/grouping/D009/'),
+#     (22222, 'A', 'output/grouping/D010/'),
+#     (54545, 'B', 'output/grouping/E001/'), # algo B
+#     (47575, 'B', 'output/grouping/E002/'), # N_test = 5000
+#     (23232, 'B', 'output/grouping/E003/'),
+#     (68685, 'B', 'output/grouping/E004/'),
+#     (35737, 'B', 'output/grouping/E005/'),
+#     (38444, 'B', 'output/grouping/E006/'),
+#     (24953, 'B', 'output/grouping/E007/'),
+#     (45353, 'B', 'output/grouping/E008/'),
+#     (53535, 'B', 'output/grouping/E009/'),
+#     (22222, 'B', 'output/grouping/E010/'),
+# ]
 
-main(parlist[0])
+# from multiprocessing import Pool
+# pool = Pool(6)
+# pool.map(main, parlist)
+
+# print the best one
+main((64755, 'A', 'output/grouping/A002/'))
